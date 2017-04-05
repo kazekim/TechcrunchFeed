056265ee3ccf444cdef33921be0d52e9f11cde1a
Kim <jirawat@eatigo.com>
2017-04-05 19:29:44 +0700

    Init databinding

--- TechCrunch/app/src/main/java/com/kazekim/techcrunch/MainActivity.java
+++ TechCrunch/app/src/main/java/com/kazekim/techcrunch/MainActivity.java
@@ -1,13 +1,34 @@
 package com.kazekim.techcrunch;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.kazekim.techcrunch.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    public enum ActivityPage {
+        MAIN,
+        DETAIL
+    }
+
+    private ActivityPage currentPage;
+    Toolbar toolbar;
+    TextView tvToolBarTitle;
+
+    private static int fragmentContainerResId = R.id.contentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        this.binding.toolbar.setTitle(R.string.app_name);
+
     }
 }
